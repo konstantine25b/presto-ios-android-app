d45f06af2ea6d6c95db23bbe0c63ad68a5c4e219
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2023-12-30 14:08:44 +0400

    fdgf

--- Components/Authentication/LogIn.js
+++ Components/Authentication/LogIn.js
@@ -1,61 +1,27 @@
-import { StyleSheet, View } from "react-native";
-import COLORS from "../Styles/colors.js";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import InputFields from "./LoginComponents/InputFields.js";
 import UpperSide from "./LoginComponents/UpperSide.js";
 
 export default function LogIn() {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
       <UpperSide />
       <InputFields />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  topLine: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    marginTop: 60,
-  },
-  otherOptions: { fontSize: 18, color: COLORS.mainColor },
-  logos: {
-    flexDirection: "row",
-    color: "white",
-    padding: 5,
-    marginBottom: 40,
-  },
-  img: {
-    height: 36,
-    width: 36,
-    margin: 3,
-    borderRadius: 35,
-    borderColor: "black",
-  },
-
-  createAn: { fontSize: 40, marginRight: 0 },
-  account: { fontSize: 40, marginLeft: 0 },
-  createAccount: {
-    marginBottom: "13%",
-    marginTop: "0%",
-    alignItems: "center",
-  },
   container: {
+    flex: 1,
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 20,
     paddingBottom: "17%",
     height: "100%",
-  },
-
-  text: {
-    color: "white",
-    textAlign: "center",
-  },
-
-  field: {
-    flexDirection: "row",
-    width: "90%",
   },
 });
